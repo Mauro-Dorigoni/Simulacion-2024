3e84cd75c157204a95dd21018b406c9d1413b69a
Mauro-Dorigoni <dorigonimauro@gmail.com>
2024-04-07 20:33:17 -0300

    Reinterpretacion de las graficas

--- sim_ruleta.py
+++ sim_ruleta.py
@@ -34,9 +34,10 @@ def calculo_promedio_real(valores):
     return statistics.mean(valores)
 
 #Funcion calculo varianza y desvio reales#
-def calculo_var_desv_reales(valores,prom_real):
+def calculo_var_desv_reales(valores):
     var_real=0
     desv_real=0
+    prom_real=statistics.mean(valores)
     for x in range(37):
         frec_abs,frec_rel=calculo_frecuencias(valores,x)
         var_real=var_real + (((x-prom_real)**2)*frec_rel)
@@ -61,32 +62,27 @@ for index in range(nro_corridas):
     nro_corridas_eje_x[index]=index
 
 #Declaracion de ejes y en graficas con corridas en el eje x#
-frec_rel_prom_p_corrida=[0.000 for x in range(nro_corridas)]
-valor_prom_prom_p_corrida=[0.000 for x in range(nro_corridas)]
-var_prom_p_corrida=[0.000 for x in range(nro_corridas)]
-desv_prom_p_corrida=[0.000 for x in range(nro_corridas)]
+curva_frec_rel_p_corrida=[[0.000 for x in range(nro_iteraciones)] for f in range(nro_corridas)]
+curvas_valor_prom_p_corrida=[[0.000 for x in range(nro_iteraciones)] for f in range(nro_corridas)]
+curvas_var_p_corrida=[[0.000 for x in range(nro_iteraciones)] for f in range(nro_corridas)]
+curvas_desv_prom_p_corrida=[[0.000 for x in range(nro_iteraciones)] for f in range(nro_corridas)]
 
 for j in range (nro_corridas): #El for para cada corrida#
     valores=[0 for x in range (nro_iteraciones)]
 
-    #Declaracion de ejes y en graficas con tiradas en el eje x#
-    frec_rel_real_grafica=[0.000 for x in range(nro_iteraciones)]
-    valor_prom_p_tirada=[0.000 for x in range(nro_iteraciones)]
-    var_p_tirada=[0.000 for x in range(nro_iteraciones)]
-    desv_p_tirada=[0.000 for x in range(nro_iteraciones)]
 
     for index in range(len(valores)): #El for para cada tirada#
         valores[index]=int(random.randint(0,36))
         #Calculo eje y en grafica frecuencia por tirada#
         frec_abs,frec_rel=calculo_frecuencias(valores,nro_elegido)
-        frec_rel_real_grafica[index]=frec_rel
+        curva_frec_rel_p_corrida[j][index]=frec_rel
         #Calculo eje y en grafica valor promedio por tirada#
-        valor_prom_p_tirada[index]=statistics.mean(valores)
-        var_real,desv_real=calculo_var_desv_reales(valores,valor_prom_p_tirada[index])
+        curvas_valor_prom_p_corrida[j][index]=statistics.mean(valores)
+        var_real,desv_real=calculo_var_desv_reales(valores)
         #Calculo eje y en grafica varianza por tirada#
-        var_p_tirada[index]=var_real
+        curvas_var_p_corrida[j][index]=var_real
         #Calculo eje y grafica desvio por tirada#
-        desv_p_tirada[index]=desv_real
+        curvas_desv_prom_p_corrida[j][index]=desv_real
     
     #Valores esperados por tirada#
     frec_rel_esperada_grafica=[(1/37) for x in range(nro_iteraciones)]
@@ -95,92 +91,67 @@ for j in range (nro_corridas): #El for para cada corrida#
     var_esperada_tirada_grafica=[var_esp for x in range(nro_iteraciones)]
     desv_esperad0_tirada_grafica=[dev_esp for x in range(nro_iteraciones)]
 
-    #Eje x en Graficas por tirada#
-    nro_tiradas=[0 for x in range(nro_iteraciones)]
-    for index in range (len(nro_tiradas)):
-        nro_tiradas[index]=index
 
-    #Graficas de Tiradas#
-    if(j==0):
-        f=plt.figure(1)
-        plt.plot(nro_tiradas,frec_rel_real_grafica, label='frn',color='red')
-        plt.plot(nro_tiradas,frec_rel_esperada_grafica,label='fre', color='blue')
-        plt.title('Frecuencia relativa (real y esperada) vs numero de tiradas')
-        plt.xlabel('numero tiradas')
-        plt.ylabel('frecuencia relativa')
-        plt.legend()
-        f.savefig('grafica_frecuencia_por_tirada.png')
-        p=plt.figure(2)
-        plt.plot(nro_tiradas,valor_prom_p_tirada, label='vpn',color='red')
-        plt.plot(nro_tiradas,prom_esperado_tirada_grafica,label='vpe', color='blue')
-        plt.title('Valor promedio (real y esperado) vs numero de tiradas')
-        plt.xlabel('numero tiradas')
-        plt.ylabel('Valor promedio')
-        plt.legend()
-        p.savefig('grafica_promedio_por_tirada.png')
-        d=plt.figure(3)
-        plt.plot(nro_tiradas,desv_p_tirada, label='vd',color='red')
-        plt.plot(nro_tiradas,desv_esperad0_tirada_grafica,label='vde', color='blue')
-        plt.title('Desvio (real y esperado) vs numero de tiradas')
-        plt.xlabel('numero tiradas')
-        plt.ylabel('Desvio')
-        plt.legend()
-        d.savefig('grafica_desvio_por_tirada.png')
-        v=plt.figure(4)
-        plt.plot(nro_tiradas,var_p_tirada, label='vvn',color='red')
-        plt.plot(nro_tiradas,var_esperada_tirada_grafica,label='vve', color='blue')
-        plt.title('Varianza (real y esperada) vs numero de tiradas')
-        plt.xlabel('numero tiradas')
-        plt.ylabel('Varianza')
-        plt.legend()
-        v.savefig('grafica_varianza_por_tirada.png')
     
-    #Calculo eje y en grafica frecuencia promedio por corrida#
-    frec_rel_prom_p_corrida[j]=statistics.mean(frec_rel_real_grafica)
-    #Calculo eje y en grafica valor promedio promedio por corrida#
-    valor_prom_prom_p_corrida[j]=statistics.mean(valor_prom_p_tirada)
-    #Calculo eje y en grafica varianza promedio por corrida#
-    var_prom_p_corrida[j]=statistics.mean(var_p_tirada)
-    #Calculo eje y en grafica desvio promedio por corrida#
-    desv_prom_p_corrida[j]=statistics.mean(desv_p_tirada)
+#Eje x en Graficas por tirada#
+nro_tiradas=[0 for x in range(nro_iteraciones)]
+for index in range (len(nro_tiradas)):
+    nro_tiradas[index]=index    
+
 
 #Valores esperados promedio por corrida#
-frec_rel_prom_esp_p_corrida=[(1/37) for x in range(nro_corridas)]
-prom_prom_esp_p_corrida=[18 for x in range(nro_corridas)]
+frec_rel_prom_esp_p_corrida=[(1/37) for x in range(nro_iteraciones)]
+prom_prom_esp_p_corrida=[18 for x in range(nro_iteraciones)]
 var_esp,desv_esp=calculo_var_desv_esperados()
-var_prom_esp_p_corrida=[var_esp for x in range(nro_corridas)]
-desv_prom_esp_p_corrida=[desv_esp for x in range(nro_corridas)]
+var_prom_esp_p_corrida=[var_esp for x in range(nro_iteraciones)]
+desv_prom_esp_p_corrida=[desv_esp for x in range(nro_iteraciones)]
 
 #Graficas por corridas#
-fp=plt.figure(5)
-plt.plot(nro_corridas_eje_x,frec_rel_prom_p_corrida, label='fprn',color='red')
-plt.plot(nro_corridas_eje_x,frec_rel_prom_esp_p_corrida,label='fpre', color='blue')
-plt.title('Frecuencia relativa promedio (real y esperada) vs numero de corridas')
-plt.xlabel('numero corridas')
-plt.ylabel('frecuencia relativa promedio')
+f=plt.figure()
+plt.plot(nro_tiradas,frec_rel_prom_esp_p_corrida, label='fre',color='blue')
+for i in range(nro_corridas): 
+    plt.plot(nro_tiradas,curva_frec_rel_p_corrida[i],label='frn'+str(i+1),color='red')
 plt.legend()
-fp.savefig('frecuencia_promedio_corrida.png')
-vpp=plt.figure(6)
-plt.plot(nro_corridas_eje_x,valor_prom_prom_p_corrida, label='vppn',color='red')
-plt.plot(nro_corridas_eje_x,prom_prom_esp_p_corrida,label='vppe', color='blue')
-plt.title('Valor promedio promedio (real y esperado) vs numero de corridas')
-plt.xlabel('numero corridas')
-plt.ylabel('Valor promedio promedio')
+f.savefig('curvas_frecuencia_por_corrida.jpg')
+
+p=plt.figure()
+plt.plot(nro_tiradas,prom_prom_esp_p_corrida, label='vpe',color='blue')
+for i in range(nro_corridas): 
+    plt.plot(nro_tiradas,curvas_valor_prom_p_corrida[i],label='vpn'+str(i+1),color='red')
 plt.legend()
-vpp.savefig('valor_promedio_promedio_corrida.png')
-dp=plt.figure(7)
-plt.plot(nro_corridas_eje_x,desv_prom_p_corrida, label='vdp',color='red')
-plt.plot(nro_corridas_eje_x,desv_prom_esp_p_corrida,label='vdpe', color='blue')
-plt.title('Desvio promedio (real y esperado) vs numero de corridas')
-plt.xlabel('numero corridas')
-plt.ylabel('Desvio promedio')
+p.savefig('curvas_promedio_por_corrida.jpg')
+
+v=plt.figure()
+plt.plot(nro_tiradas,var_prom_esp_p_corrida, label='vve',color='blue')
+for i in range(nro_corridas): 
+    plt.plot(nro_tiradas,curvas_var_p_corrida[i],label='vvn'+str(i+1),color='red')
 plt.legend()
-dp.savefig('desvio_promedio_corrida.png')
-vp=plt.figure(8)
-plt.plot(nro_corridas_eje_x,var_prom_p_corrida, label='vvpn',color='red')
-plt.plot(nro_corridas_eje_x,var_prom_esp_p_corrida,label='vvpe', color='blue')
-plt.title('Varianza promedio (real y esperada) vs numero de corridas')
-plt.xlabel('numero corridas')
-plt.ylabel('Varianza promedio')
+v.savefig('curvas_varianza_por_corrida.jpg')
+
+d=plt.figure()
+plt.plot(nro_tiradas,desv_prom_esp_p_corrida, label='vde',color='blue')
+for i in range(nro_corridas): 
+    plt.plot(nro_tiradas,curvas_desv_prom_p_corrida[i],label='vdn'+str(i+1),color='red')
 plt.legend()
-vp.savefig('varianza_promedio_corrida.png')
+d.savefig('curvas_desvio_por_corrida.jpg')
+
+#Graficas por tirada#
+def graficas_por_tirada(esperada, datos, etiqueta, nombre_archivo):
+    f=plt.figure()
+    plt.plot(nro_tiradas,esperada, label=etiqueta+'e', color='blue')
+    plt.plot(nro_tiradas, datos, label=etiqueta+'n',color='red')
+    plt.legend()
+    f.savefig(nombre_archivo+'.jpg')
+
+for x in range(nro_corridas):
+    graficas_por_tirada(frec_rel_prom_esp_p_corrida, curva_frec_rel_p_corrida[x], 'fr', 'grafica_frecuencia_por_tirada'+str(x))
+
+for x in range(nro_corridas):
+    graficas_por_tirada(prom_prom_esp_p_corrida, curvas_valor_prom_p_corrida[x], 'vp', 'grafica_promedio_por_tirada'+str(x))
+
+for x in range(nro_corridas):
+    graficas_por_tirada(var_prom_esp_p_corrida, curvas_var_p_corrida[x], 'vv', 'grafica_varianza_por_tirada'+str(x))
+
+for x in range(nro_corridas):
+    graficas_por_tirada(desv_prom_esp_p_corrida, curvas_desv_prom_p_corrida[x], 'vd', 'grafica_desvio_por_tirada'+str(x))
+    
