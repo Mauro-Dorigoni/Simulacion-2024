93876f7218522972ae0a93b150ccbe64ae4a548b
Mauro-Dorigoni <dorigonimauro@gmail.com>
2024-05-02 13:14:40 -0300

    Estrategia Martingala

--- TP1.2/sim_matematica_apuestas_ruleta.py
+++ TP1.2/sim_matematica_apuestas_ruleta.py
@@ -6,7 +6,7 @@ import sys
 import matplotlib as plt
 import matplotlib.pyplot as plt
 
-#Funcion calculo de promedio esperado#
+"""#Funcion calculo de promedio esperado#
 def calculo_promedio_esperado():
     return 18
 
@@ -42,11 +42,11 @@ def calculo_var_desv_reales(valores):
         frec_abs,frec_rel=calculo_frecuencias(valores,x)
         var_real=var_real + (((x-prom_real)**2)*frec_rel)
     desv_real=math.sqrt(var_real)
-    return var_real,desv_real
+    return var_real,desv_real """
 
 #compurebo que el programa en CLI se use como se debe#
-if len(sys.argv)!=7 or sys.argv[1]!="-c" or sys.argv[3]!="-n" or sys.argv[5]!="-e":
-    print("Uso: python sim_ruleta.py -c <nro_corridas> -n <nro_iteraciones> -e <nro_elegido> -s <estrategia [m|d|f|o]> -a <tipo_capital [i|f]>")
+if  (len(sys.argv)!=11) or sys.argv[1]!="-c" or sys.argv[3]!="-n" or sys.argv[5]!="-e" or sys.argv[7]!="-s" or sys.argv[9]!="-a" or (sys.argv[8] not in ["m","d","f","o"]) or (sys.argv[10] not in ["i","f"]):
+    print("Uso: python sim_matematica_apuestas_ruleta.py -c <nro_corridas> -n <nro_iteraciones> -e <nro_elegido> -s <estrategia [m|d|f|o]> -a <tipo_capital [i|f]>")
     sys.exit(1)
 
 #Obtengo el nro de iteraciones y el nro elegido de la CLI#
@@ -56,8 +56,59 @@ nro_elegido=int(sys.argv[6])
 estrategia=(sys.argv[8])
 tipo_capital=(sys.argv[10])
 
+def martingala_sig_paso(apuesta,paso,ganancias,salida):
+    negro=[15,4,2,17,6,13,11,8,10,24,33,20,31,22,29,28,35,26]
+    if salida in negro:
+        ganancias=ganancias+apuesta*2
+        apuesta=1
+        return 1,ganancias,apuesta
+    else:
+        ganancias=ganancias-apuesta
+        apuesta=apuesta*2
+        if paso==5:
+            apuesta=1 
+            return 1,ganancias,apuesta
+        return paso+1,ganancias,apuesta
+
+def martingala(nro_tiradas,tipo_capital,banca):
+    flujo_caja=[0 for x in range(nro_tiradas+1)]
+    nro_tiradas_eje_x=[0 for x in range(nro_tiradas+1)]
+    for index in range(nro_tiradas+1):
+        nro_tiradas_eje_x[index]=index
+    paso_a_usar=1
+    apuesta=1
+    ejex=nro_tiradas
+    if tipo_capital=="f":
+        flujo_caja[0]=banca
+        ganancias=banca
+        for x in range (nro_tiradas):
+            if ganancias<=0: break
+            salida=int(random.randint(0,36))
+            paso_a_usar,ganancias,apuesta=martingala_sig_paso(apuesta,paso_a_usar,ganancias,salida) 
+            flujo_caja[x+1]=ganancias
+            ejex=x
+
+    if tipo_capital=="i":
+        flujo_caja[0]=0
+        ganancias=0
+        for x in range (nro_tiradas):
+            salida=int(random.randint(0,36))
+            paso_a_usar,ganancias,apuesta=martingala_sig_paso(apuesta,paso_a_usar,ganancias,salida) 
+            flujo_caja[x+1]=ganancias
+    print(flujo_caja)
+    f=plt.figure()
+    plt.plot(nro_tiradas_eje_x[:ejex],flujo_caja[:ejex],label="fc",color="red")
+    plt.legend()
+    f.savefig("prueba.jpg")
+
 
 #Programa Principal#
+martingala(nro_iteraciones,tipo_capital,10)
+print("qcyo")
+
+
+
+""" #Programa Principal#
 #Eje x en graficas por corrida#
 nro_corridas_eje_x=[0 for x in range(nro_corridas)]
 for index in range(nro_corridas):
@@ -106,6 +157,5 @@ frec_rel_prom_esp_p_corrida=[(1/37) for x in range(nro_iteraciones)]
 prom_prom_esp_p_corrida=[18 for x in range(nro_iteraciones)]
 var_esp,desv_esp=calculo_var_desv_esperados()
 var_prom_esp_p_corrida=[var_esp for x in range(nro_iteraciones)]
-desv_prom_esp_p_corrida=[desv_esp for x in range(nro_iteraciones)]
-
+desv_prom_esp_p_corrida=[desv_esp for x in range(nro_iteraciones)] """
     
